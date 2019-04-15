b59eb6df6fdd01335babdc1e9fc35de332c50c5f
feven <feventsegay6@gmail.com>
2019-04-15 20:28:01 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,5 +2,5 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 def introduction_with_language(name,language)
-  puts ""
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
